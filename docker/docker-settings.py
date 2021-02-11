43461beee398fc477f21302169b75dbd9c65adff
Eduardo Miguel Botía Domingo <edubotdom@alum.us.es>
2021-02-11 18:22:19 +0100

    Implementadas correcciones sobre Docker

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,7 +5,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
